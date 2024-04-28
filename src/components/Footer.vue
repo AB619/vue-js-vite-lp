3926cc4fe4581c6c9646dfc3c88896bb3c2dbbc2
Ankit Bhaskar <ankit_bhaskar@epam.com>
2024-04-28 22:07:29 +0530

    [module 3] UI created for netflix roulette application

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer class="footer">
+    <span class="footer__text1">netflix</span>
+    <span>roulette</span>
+  </footer>
+</template>
+
+<style scoped>
+  .footer{
+    background-color: #555555;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+  }
+  .footer > span {
+    color: #f65261;
+    font-size: 20px;
+    letter-spacing: 0.04em;
+  }
+  .footer__text1{
+    font-weight: 1000;
+  }
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,68 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+import MovieList from "./MovieList.vue";
+import Footer from "./Footer.vue";
+import SortPanel from "./SortPanel.vue";
+import Header from "./Header.vue";
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <Header />
+  <SortPanel />
+  <MovieList :movies="[{
+    name: 'The Amazing Spiderman',
+    genre: 'Action & Adventure',
+    year: 2012,
+    rating: 4.3,
+    movieLength: 128,
+    poster: 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/BgcvtsVWLQfjHD6Dr3YYgeSLYk.jpg',
+    description: 'Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents disappearance leading him directly to Oscorp and the lab of Dr. Curt Connors, his father former partner. As Spider-Man is set on a collision course with Connors alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.'
+  },
+  {
+    name: 'The Amazing Spiderman',
+    genre: 'Action & Adventure',
+    year: 2012,
+    rating: 4.3,
+    movieLength: 128,
+    poster: 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/BgcvtsVWLQfjHD6Dr3YYgeSLYk.jpg',
+    description: 'Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents disappearance leading him directly to Oscorp and the lab of Dr. Curt Connors, his father former partner. As Spider-Man is set on a collision course with Connors alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.'
+  },
+  {
+    name: 'The Amazing Spiderman',
+    genre: 'Action & Adventure',
+    year: 2012,
+    rating: 4.3,
+    movieLength: 128,
+    poster: 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/BgcvtsVWLQfjHD6Dr3YYgeSLYk.jpg',
+    description: 'Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents disappearance leading him directly to Oscorp and the lab of Dr. Curt Connors, his father former partner. As Spider-Man is set on a collision course with Connors alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.'
+  },
+  {
+    name: 'The Amazing Spiderman',
+    genre: 'Action & Adventure',
+    year: 2012,
+    rating: 4.3,
+    movieLength: 128,
+    poster: 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/BgcvtsVWLQfjHD6Dr3YYgeSLYk.jpg',
+    description: 'Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents disappearance leading him directly to Oscorp and the lab of Dr. Curt Connors, his father former partner. As Spider-Man is set on a collision course with Connors alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.'
+  },
+  {
+    name: 'The Amazing Spiderman',
+    genre: 'Action & Adventure',
+    year: 2012,
+    rating: 4.3,
+    movieLength: 128,
+    poster: 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/BgcvtsVWLQfjHD6Dr3YYgeSLYk.jpg',
+    description: 'Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents disappearance leading him directly to Oscorp and the lab of Dr. Curt Connors, his father former partner. As Spider-Man is set on a collision course with Connors alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.'
+  },
+  {
+    name: 'The Amazing Spiderman',
+    genre: 'Action & Adventure',
+    year: 2012,
+    rating: 4.3,
+    movieLength: 128,
+    poster: 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/BgcvtsVWLQfjHD6Dr3YYgeSLYk.jpg',
+    description: 'Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents disappearance leading him directly to Oscorp and the lab of Dr. Curt Connors, his father former partner. As Spider-Man is set on a collision course with Connors alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.'
+  }]" />
+  <Footer />
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
