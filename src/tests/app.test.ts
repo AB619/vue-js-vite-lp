77ec5e75ac12719f3bda5f2667410236bcddc274
Ankit Bhaskar <ankit_bhaskar@epam.com>
2024-05-28 19:05:48 +0530

    [home task] test cases updated

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -21,3 +21,32 @@ test("mount component", async () => {
   const wrapper = mount(App);
   expect(wrapper.text()).toContain("netflixroulette");
 });
+
+test("search movies", async () => {
+  const wrapper = mount(App);
+
+  await wrapper.find('input[type="text"]').setValue('The Shawshank Redemption')
+  await wrapper.get('button[type="submit"]').trigger('submit');
+  expect(wrapper.find('.card .card__info__name').text()).toContain('The Shawshank Redemption');
+
+  await wrapper.find('input[type="text"]').setValue('zzzzzzzz')
+  await wrapper.get('button[type="submit"]').trigger('submit');
+  expect(wrapper.find('.movies__empty').text()).toContain('No films found');
+});
+
+test("sort movies", async () => {
+  const wrapper = mount(App);
+
+  await wrapper.find('.rating').trigger('click');
+  expect( wrapper.find('.card .card__info__name').text() ).toContain('The Shawshank Redemption');
+});
+
+test("sort movies", async () => {
+  const wrapper = mount(App);
+
+  await wrapper.find('.card .card__info__name').trigger('click');
+  expect(wrapper.find('.banner-info').text()).toContain('The Kid');
+
+  await wrapper.find('.header__right').trigger('click');
+  expect(wrapper.find('.searchlabel'))
+});
